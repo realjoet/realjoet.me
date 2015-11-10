cbe6e27bca72634cf6e7840c3ae77b3079236dbc
Joe Toscano <realjoet9@gmail.com>
2015-11-10 11:18:47 -0700

    config file set up to work on Heroku and localhost

--- config/config.js
+++ config/config.js
@@ -28,7 +28,6 @@ var config = {
     app: {
       name: 'realjoet-me'
     },
-    // Google how to set up Node production env on Heroku
     port: process.env.PORT,
     db: process.env.MONGOLAB_URI
   }
