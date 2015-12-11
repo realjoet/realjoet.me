c3f4c31e2a23123b221c6739613ad11be626ec56
Joe Toscano <realjoet9@gmail.com>
2015-12-10 17:03:57 -0700

    styling of content heros, with titles and hover

--- public/js/modernizr-settings.js
+++ public/js/modernizr-settings.js
@@ -1,9 +1,7 @@
 $(function() {
   if (Modernizr.touch) {
-    $('.hero-cover > span').addClass('hero-cover__title');
-    $('.hero-cover__title').html("title");
+
   } else {
-    $('.hero-cover > span').addClass('hero-cover__target');
-    $('.hero-cover__target').html("+");
+    
   }
 });
