e3ec5726d87be5b7d9afe72e8d0a7e9f85bc7a6e
Joe Toscano <realjoet9@gmail.com>
2015-12-12 12:39:25 -0700

    mobile friendly touch-ups v1

--- public/js/modernizr-settings.js
+++ public/js/modernizr-settings.js
@@ -2,6 +2,6 @@ $(function() {
   if (Modernizr.touch) {
     $('.display-section__content-hero').addClass('touch');
   } else {
-    
+    $('.display-section__content-hero').removeClass('touch');
   }
 });
