9d6b9c9cd7ce8334b30215a5747f09c2a9407ae6
Joe Toscano <realjoet9@gmail.com>
2015-12-12 13:19:53 -0700

    fluid responsive across viewports, still needs touch-ups on treasure images and content-hero images

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+//! Sets Display Section position across browsers, on resize
+function displaySectionPos() {
+  var $statement = $('.statement-container');
+  var $statementPos = $statement.position();
+  var $statementHeight = $statement.outerHeight();
+
+  if ($(window).width() < 786) {
+    $('.display-section-container').css({"top": $statementPos.top + $statementHeight + 15});
+  } else {
+    $('.display-section-container').css({"top": "110%"});
+  }
+}
+
+$(function(){
+  displaySectionPos();
+
+  $(window).resize(function(){
+    displaySectionPos();
+  })
+})
